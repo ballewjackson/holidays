2086ce6e416fb1af91a4fd88f0f70a6454c6988f
ballewjackson <ballewjackson@gmail.com>
2024-07-11 15:23:45 -0500

    Updated holidays to reflect known TFMC holidays and added gausian easter calculation.

--- holidays.py
+++ holidays.py
@@ -1,15 +1,34 @@
 import pandas as pd
+import math
+
+def gaussEaster(Y):
+    A = Y % 19
+    B = Y % 4
+    C = Y % 7
+    P = math.floor(Y / 100)
+    Q = math.floor((13 + 8 * P) / 25)
+    M = (15 - Q + P - P // 4) % 30
+    N = (4 + P - P // 4) % 7
+    D = (19 * A + M) % 30
+    E = (2 * B + 4 * C + 6 * D + N) % 7
+    days = (22 + D + E)
+    if ((D == 29) and (E == 6)):
+        return f"{Y}-04-19"
+    elif ((D == 28) and (E == 6)):
+        return f"{Y,}-04-18"
+    else:
+        if (days > 31):
+            return f"{Y}-04-{days - 31}"
+        else:
+            return f"{Y}-03-{days}"
 
 def CheckHoliday(row):
-    # first let's do all that are the fixed date
-    # dec 31
-    # jan 1
-    # june 19 ???
-    # july 4
-    # nov 11
-    # dec 24
-    # dec 25
-    fixedDates = ["01-01", "06-19", "07-04", "11-11", "12-24", "12-25", "12-31"]
+    """
+    Returns a string : {"easter", "previous_day", "current_day", "next_day"} based on whether the date of the input row is a Holiday
+    """
+    if row["dayOfWeekName"] == "Sunday" and (row["MMDD"][:2] == "03" or row["MMDD"][:2] == "04") and gaussEaster(int(row["Date"].strftime('%Y-%m-%d')[:4])) == row["Date"].strftime('%Y-%m-%d'):
+        return "easter"
+    fixedDates = ["01-01", "07-04", "12-24", "12-25"]
     for date in fixedDates:
         if row["MMDD"] == date:
             if row["dayOfWeekName"] == "Saturday":
@@ -17,26 +36,14 @@ def CheckHoliday(row):
             if row["dayOfWeekName"] == "Sunday":
                 return "next_day"
             return "current_day"
-    # variable dates
-    # 3rd Monday of Jan
-    # 3rd Monday of Feb
-    # Last Monday of May
-    # 1st monday of sept
-    # 2nd monday of Oct
-    # 4th thursday of nov
     conditions = (
-        (row["MMDD"][:2] == "01" or row["MMDD"][:2] == "02" ) and row["dayOfWeekName"] == "Monday" and row["weekOfMonth"] == 3,
         row["MMDD"][:2] == "05" and row["dayOfWeekName"] == "Monday" and int(row["MMDD"][3:]) > 24,
         row["MMDD"][:2] == "09" and row["dayOfWeekName"] == "Monday" and row["weekOfMonth"] == 1,
-        row["MMDD"][:2] == "10" and row["dayOfWeekName"] == "Monday" and row["weekOfMonth"] == 2,
         row["MMDD"][:2] == "11" and row["dayOfWeekName"] == "Thursday" and row["weekOfMonth"] == 4,
         row["MMDD"][:2] == "11" and row["dayOfWeekName"] == "Friday" and row["weekOfMonth"] == 4,
     )
     if any(conditions):
         return "current_day"
-    # Need to check multiple days for Thanksgiving, Dec 31, Dec 24, Election Day, Easter Monday, Good Friday
-    # I asked chelsea
-
     return ""
             
 
@@ -56,6 +63,9 @@ df["dayOfWeekName"] = df["Date"].dt.day_name()
 for index in range(len(df)):
     holidayStatus = CheckHoliday(df.iloc[index])
     match holidayStatus:
+        case "easter":
+            df.at[index-2, "holiday"] = True
+            df.at[index, "holiday"] = True
         case "previous_day":
             df.at[index-1, "holiday"] = True
         case "current_day":
@@ -67,5 +77,4 @@ for index in range(len(df)):
         case _:
             raise("Exemption in Match based on return of CheckHoliday()")
 
-print(df)
 df.to_csv(r"C:\Users\jballew\OneDrive - Digicorner\Documents\holidays\holidays.csv", sep=",", header=True, index=False)
